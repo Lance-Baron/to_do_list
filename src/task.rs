3898c9c8421856ae70203c72cc209f540fc7679f
Lance-Baron <lbaron082@gmail.com>
2026-05-15 02:18:42 -0600

    Added another constructor, with all fields

--- src/task.rs
+++ src/task.rs
@@ -18,7 +18,16 @@ pub mod task {
             due_date,
         }
     }
-
+    pub fn make_task_full(name: String,description:String, source:String, topic:String, due_date: Date) -> Task {
+        Task {
+            name,
+            description,
+            source,
+            topic,
+            due_date,
+        }
+    }
+    
     #[cfg(test)]
     mod tests {
         use super::*;
@@ -33,6 +42,18 @@ pub mod task {
             assert_eq!(task1.description, "", "Description does not match");
             assert_eq!(task1.source, "Unknown", "Sources does not match");
             assert_eq!(task1.topic, "Unknown", "Topics does not match");
+            assert_eq!(task1.due_date, date, "Dates do not match");
+        }
+        #[test]
+        fn test_make_task_full() {
+            let date =
+                Date::from_calendar_date(2026, Month::April, 4).expect("Failed to make Date");
+            let task1 = make_task_full(String::from("Do HW2"), String::from("Finish up the filters problems from Chapter 11.5"), String::from("Canvas"),String::from("ECE202"),date);
+
+            assert_eq!(task1.name, "Do HW2", "Names do not match");
+            assert_eq!(task1.description, "Finish up the filters problems from Chapter 11.5", "Description does not match");
+            assert_eq!(task1.source, "Canvas", "Sources does not match");
+            assert_eq!(task1.topic, "ECE202", "Topics does not match");
             assert_eq!(task1.due_date, date, "Dates do not match");
         }
     }
