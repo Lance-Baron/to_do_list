569149758967f6ba21e319c483011cda2034964a
Lance-Baron <lbaron082@gmail.com>
2026-05-15 02:44:35 -0600

    Changed Source to be an Enum

--- src/task.rs
+++ src/task.rs
@@ -1,10 +1,17 @@
 pub mod task {
     use time::Date;
     use time::Month;
+
+    #[derive(PartialEq,Debug)]
+    pub enum TaskSource {
+        Canvas,
+        Unknown,
+    }
+
     pub struct Task {
         pub name: String,
         pub description: String,
-        pub source: String,
+        pub source: TaskSource,
         pub topic: String,
         pub due_date: Date,
     }
@@ -13,12 +20,12 @@ pub mod task {
         Task {
             name,
             description: String::from(""),
-            source: String::from("Unknown"),
+            source: TaskSource::Unknown,
             topic: String::from("Unknown"),
             due_date,
         }
     }
-    pub fn make_task_full(name: String,description:String, source:String, topic:String, due_date: Date) -> Task {
+    pub fn make_task_full(name: String,description:String, source:TaskSource, topic:String, due_date: Date) -> Task {
         Task {
             name,
             description,
@@ -40,7 +47,7 @@ pub mod task {
 
             assert_eq!(task1.name, "Do HW 3", "Names do not match");
             assert_eq!(task1.description, "", "Description does not match");
-            assert_eq!(task1.source, "Unknown", "Sources does not match");
+            assert_eq!(task1.source, TaskSource::Unknown, "Sources does not match");
             assert_eq!(task1.topic, "Unknown", "Topics does not match");
             assert_eq!(task1.due_date, date, "Dates do not match");
         }
@@ -48,11 +55,11 @@ pub mod task {
         fn test_make_task_full() {
             let date =
                 Date::from_calendar_date(2026, Month::April, 4).expect("Failed to make Date");
-            let task1 = make_task_full(String::from("Do HW2"), String::from("Finish up the filters problems from Chapter 11.5"), String::from("Canvas"),String::from("ECE202"),date);
+            let task1 = make_task_full(String::from("Do HW2"), String::from("Finish up the filters problems from Chapter 11.5"), TaskSource::Canvas,String::from("ECE202"),date);
 
             assert_eq!(task1.name, "Do HW2", "Names do not match");
             assert_eq!(task1.description, "Finish up the filters problems from Chapter 11.5", "Description does not match");
-            assert_eq!(task1.source, "Canvas", "Sources does not match");
+            assert_eq!(task1.source, TaskSource::Canvas, "Sources does not match");
             assert_eq!(task1.topic, "ECE202", "Topics does not match");
             assert_eq!(task1.due_date, date, "Dates do not match");
         }
